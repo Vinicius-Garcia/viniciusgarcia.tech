d45bfd720c74c0866c8324e874ac11f42e8aff95
Vinicius-Garcia <viniciusgarcia1300@gmail.com>
2022-06-08 08:59:39 -0300

    refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,20 @@
-import { useState } from 'react'
-import { Sidebar } from './components/sidebar/Sidebar'
-import {Home} from './components/home/Home';
-import './styles.css'
-import './global.css'
+import { Sidebar } from "./components/sidebar/Sidebar";
+import { Home } from "./components/home/Home";
+import "./styles.css";
+import "./global.css";
 function App() {
   return (
     <>
-    <div className="wrapper">
-      <div className="sidebars">
-        <Sidebar/>
+      <div className="wrapper">
+        <div className="sidebars">
+          <Sidebar />
+        </div>
+        <div className="content">
+          <Home />
+        </div>
       </div>
-      <div className="content">
-        <Home/>
-      </div>
-    </div>
-    
-  
- 
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -2,28 +2,29 @@ import imagemLogo from '../../assets/perfil.jpg'
 import twitter from '../../assets/Frame.svg'
 import github from '../../assets/githab.svg'
 import linkedin from '../../assets/linkedin.svg'
+import { Link } from 'react-router-dom';
 import './styles.css'
 
-export function Sidebar(){
-  return(
+export function Sidebar() {
+  return (
     <div className="container">
       <div className="sidebar">
-      <img src={imagemLogo} alt="foto pessoal" className="imgLogo" />
-      <h2>Vinicius da Silva Garcia</h2>
-      <small>Desenvolvedor Web</small>
-      <div className="buttons">
-      <a href="#">Home</a>
-      <a href="#">Sobre</a>
-      <a href="#">Projetos</a>
+        <img src={imagemLogo} alt="foto pessoal" className="imgLogo" />
+        <h2>Vinicius da Silva Garcia</h2>
+        <small>Desenvolvedor Web</small>
+        <div className="buttons">
+          <Link to="/" >Home</Link>
+          <Link to="/sobre">Sobre</Link>
+          <Link to="/projetos">Projetos</Link>
+        </div>
+        <div className="social">
+          <a href="https://www.linkedin.com/in/vinicius-da-silva-garcia/" target="_blank"><img src={linkedin} alt="icone do linkedin" /></a>
+          <a href="https://www.github.com/Vinicius-Garcia" target="_blank"><img src={github} alt="icone do github" /></a>
+          <a href="https://www.twitter.com/viniciusdsg1" target="_blank"><img src={twitter} alt="icone do twitter" /></a>
+        </div>
+
       </div>
-      <div className="social">
-           <a href="https://www.linkedin.com/in/vinicius-da-silva-garcia/" target="_blank"><img src={linkedin} alt="icone do linkedin"  /></a>
-           <a  href="https://www.github.com/Vinicius-Garcia" target="_blank"><img src={github} alt="icone do github" /></a>
-           <a  href="https://www.twitter.com/viniciusdsg1" target="_blank"><img src={twitter} alt="icone do twitter"/></a>
-      </div>
-      
-      </div>
-      
+
     </div>
   )
 }
